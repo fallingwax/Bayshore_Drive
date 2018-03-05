ea763ee618791885767753dd10cc4394a985a5c9
fallingwax <33186063+fallingwax@users.noreply.github.com>
2018-03-05 14:00:17 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Bayshore Drive Band</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+      <script src='https://www.google.com/recaptcha/api.js'></script>
+  </head>
+  <body>    
+<nav class="navbar navbar-expand-md navbar-light bg-faded p-2 mb-1">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Bayshore Drive</a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">People Are Talking</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Book Us!</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Calendar</a>
+      </li>      
+    </ul>
+  </div>
+    <div class="float-right">
+        <a class="p-2" href="https://www.facebook.com/bayshoredrivede/"><img class="social-media-logos" src="img/FB-f-Logo__blue_29.png"></a>
+        <a class="p-2" href="https://twitter.com/BayshoreDriveDE"><img class="social-media-logos" src="img/Twitter_Social_Icon_Rounded_Square_Color.png"></a>
+        <a class="p-2" href="https://www.instagram.com/bayshoredrivede/"><img class="social-media-logos" src="img/glyph-logo_May2016.png"></a>
+    </div>    
+</nav>    
+<div class="jumbotron h-800" id="jumbotron1">
+  <h1 class="display-4 text-center">Bayshore Drive</h1>
+  <p class="lead"></p>
+  <hr class="my-4">
+  <p class="text-center">Wilmington's newest Party Band!</p>
+</div>
+<div class="container-fluid h-800">
+    <h2 class="text-center p-4">About Us</h2>
+    <div class="container">
+    <p class="text-center">Bayshore Drive is a 5-piece band from Wilmington, DE.We are a party band with styles ranging from Classic Rock, Funk, Pop and Alternative.We are available for weddings, special events and weekend performances at local bars and restaurants.</p> 
+<!--    This section for videos    -->
+    <div class="row" id="videos">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="text-center">
+                    <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frobmcfarland05%2Fvideos%2F10101985876993832%2F&show_text=0&width=267" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+                </div>     
+                <div class="card-body">
+                    <h5 class="card-title">Bayshore Drive Live</h5>
+                    <p class="card-text">Ron's birthday party celebration</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="text-center">
+                     <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Frobmcfarland05%2Fvideos%2F10101804108524302%2F&show_text=0&width=267" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+                </div>     
+                <div class="card-body">
+                    <h5 class="card-title">Bayshore Drive Live in Studio</h5>
+                    <p class="card-text">Another Brick in the Wall</p>
+                </div>
+            </div>
+        </div>
+        
+    </div>    
+</div>
+</div>    
+<div class="jumbotron h-800" id="jumbotron2">
+  <h1 class="display-4">People Are Talking!</h1>
+  <p class="lead"></p>
+  <hr class="my-4">
+  <p>"Bayshore Drive had everyone on the floor dancing. High energy, great music!" - Christine</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+<div class="container-fluid h-800">
+    <h2 class="text-center p-4">Book Us!</h2>
+    <div class="container">
+        <form method="post">
+            <div class="form-group row">
+                <div class="col-sm-6">
+                    <label for="firstName">First Name</label>
+                    <input type="text" class="form-control" id="firstName" placeholder="">
+                </div>
+                <div class="col-sm-6">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" class="form-control" id="lastName" placeholder="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-8">
+                    <label for="emailAddress">Email Address</label>
+                    <input type="email" class="form-control" id="emailAddress" placeholder="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-8">
+                    <label for="address1">Address 1</label>
+                    <input type="text" class="form-control" id="address1" placeholder="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-8">
+                    <label for="address2">Address 2</label>
+                    <input type="text" class="form-control" id="address2" placeholder="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-6">
+                    <label for="city">City</label>
+                    <input type="text" class="form-control" id="city" placeholder="">
+                </div>
+                <div class="col-sm-2">
+                    <label for="state">State</label>
+                    <select class="form-control" id="state">
+                        <option>DE</option>
+                        <option>MD</option>
+                        <option>NJ</option>
+                        <option>NY</option>
+                        <option>PA</option>
+                        <option>VA</option>
+                    </select>    
+                </div>
+                <div class="col-sm-4">
+                    <label for="zipCode">Zip Code</label>
+                    <input type="text" class="form-control" id="zipCode" placeholder="">
+                </div>
+            </div>    
+            <div class="form-group row">    
+                <div class="col-sm-6">
+                    <label for="phoneNumber">Phone Number</label>
+                    <input type="phone" class="form-control" id="phoneNumber" placeholder="">
+                </div>
+                <div class="col-sm-2">
+                    <label for="extension">Ext</label>
+                    <input type="text" class="form-control" id="extension" placeholder="">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-8">
+                    <label for="eventType">Event Type</label>
+                    <select class="form-control" id="eventType">
+                        <option>Wedding</option>
+                        <option>Graduation</option>
+                        <option>Birthday</option>
+                        <option>Celebration</option>
+                        <option>Bar or Restraurant</option>
+                        <option>Other</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-8">
+                    <label for="eventLocation">Location Of Event</label>
+                    <input type="text" class="form-control" id="eventLocation">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-4">
+                    <label for="dateOfEvent">Date of Event</label>
+                    <input type="date" class="form-control" id="dateOfEvent">
+                </div>
+                <div class="col-sm-2">
+                    <label for="startTime">Start Time</label>
+                    <input type="time" class="form-control" id="startTime">
+                </div>
+                <div class="col-sm-2">
+                    <label for="endTime">End Time</label>
+                    <input type="time" class="form-control" id="endTime">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-8">
+                    <label for="notes">Event Notes</label>
+                    <textarea class="form-control input-lg" id="notes"></textarea>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-auto">
+                    <div class="g-recaptcha" data-sitekey="6LdZpkkUAAAAABl_f0s4ZWpbZ608F4Wq_O1goDDv"></div>
+                </div>
+            </div>    
+            <div class="form-group row">
+                <div class="col-sm-2">
+                    <button class="btn btn-primary"type="submit" id="submit">Submit</button>
+                </div>     
+            </div>
+            
+        </form>
+    </div>
+    
+</div>
+    <!-- Optional JavaScript -->
+      <script> $('#submit').on('click', function(event) {   
+              
+              event.preventDefault();
+              
+              
+
+          }
+      </script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+      
+  </body>
+</html>
